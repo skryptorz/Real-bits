12d06f2ca5fa60715505304a3871c4cde8768472
sarmishta <bsarmishta@gmail.com>
2018-05-04 14:56:39 +0000

    added route for user dashboard

--- real-bits/src/index.js
+++ real-bits/src/index.js
@@ -4,6 +4,7 @@ import SiginUp from './components/SignUp';
 import Login from './components/Login';
 import App from './components/App';
 import Home from './components/Home';
+import UserDashboard from './components/UserDashboard';
 import createHistory from 'history/createBrowserHistory';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
@@ -13,11 +14,12 @@ ReactDOM.render(
   
   <Router history={history}>
   <div>
-   <Route path="/" exact strict component={Home} />
-    
-	<Route path="/#login" exact strict component={Login}/>
+  <Route path="/" exact strict component={Home} />
+  <Route path="/#login" exact strict component={Login}/>
 	<Route path="/#signup" exact strict component={SiginUp} />
-	<Route path="/App" exact strict component={App} />
+	<Route path="/dashboard/:email" exact strict component={UserDashboard} />
+	<Route path="/App" exact strict component={App}/>
+	
 	</div>
   </Router>
  
